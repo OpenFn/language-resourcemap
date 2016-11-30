1fbe6c1191bca62cc97b188f3458b01f8738bb81
taylordowns2000 <downs.taylor@gmail.com>
2016-11-30 12:01:45 +0700

    concept for posting new sites

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -1,14 +1,6 @@
-import {
-  execute as commonExecute,
-  expandReferences
-} from 'language-common';
-import {
-  post,
-  put
-} from './Client';
-import {
-  resolve as resolveUrl
-} from 'url';
+import { execute as commonExecute, expandReferences } from 'language-common';
+import request from 'request'
+import { resolve as resolveUrl } from 'url';
 
 /** @module Adaptor */
 
@@ -48,243 +40,26 @@ export function execute(...operations) {
  * @param {object} eventData - Payload data for the event
  * @returns {Operation}
  */
-export function event(eventData) {
+export function submitSite(siteData) {
 
   return state => {
-    const body = expandReferences(eventData)(state);
+    const body = expandReferences(siteData)(state);
+
+    const collection_id = body.collection_id
 
     const {
       username,
       password,
-      apiUrl
+      hostUrl
     } = state.configuration;
 
-    const url = resolveUrl(apiUrl + '/', 'api/events')
+    // /api/collections/:collection_id/sites.json
+    const url = resolveUrl(hostUrl + '/', 'api/collections/' + collection_id + "/sites.json")
 
-    console.log("Posting event:");
+    console.log("Submitting site:");
     console.log(body)
 
-    return post({
-        username,
-        password,
-        body,
-        url
-      })
-      .then((result) => {
-        console.log("Success:", result);
-        return {...state,
-          references: [result, ...state.references]
-        }
-      })
-
-  }
-}
-
-/**
- * Send data values using the dataValueSets resource
- * @example
- * execute(
- *   dataValueSet(data)
- * )(state)
- * @constructor
- * @param {object} data - Payload data for the data value set
- * @returns {Operation}
- */
-export function dataValueSet(data) {
-
-  return state => {
-    const body = expandReferences(data)(state);
-
-    const {
-      username,
-      password,
-      apiUrl
-    } = state.configuration;
-
-    const url = resolveUrl(apiUrl + '/', 'api/dataValueSets')
-
-    console.log("Posting data value set:");
-    console.log(body)
-
-    return post({
-        username,
-        password,
-        body,
-        url
-      })
-      .then((result) => {
-        console.log("Success:", result);
-        return {...state,
-          references: [result, ...state.references]
-        }
-      })
-
-  }
-}
-
-/**
- * Create a "dataElement" pairing for resourcemap.
- * @example
- * execute(
- *   dataElement(key, value)
- * )(state)
- * @constructor
- * @param {object} key - Payload data for the Data Element key
- * @param {object} value - Payload data for the Data Element value
- * @returns {Operation}
- */
-export function dataElement(key, value) {
-  return {
-    "dataElement": key,
-    "value": value
-  }
-}
-
-/**
- * Create one or many new Tracked Entity Instances
- * @example
- * execute(
- *   createTEI(data)
- * )(state)
- * @constructor
- * @param {object} data - Payload data for new tracked entity instance(s)
- * @returns {Operation}
- */
-export function createTEI(data) {
-
-  return state => {
-
-    const body = expandReferences(data)(state);
-
-    const {
-      username,
-      password,
-      apiUrl
-    } = state.configuration;
-
-    const url = resolveUrl(apiUrl + '/', 'api/trackedEntityInstances')
-
-    console.log("Posting tracked entity instance data:");
-    console.log(body)
-
-    return post({
-        username,
-        password,
-        body,
-        url
-      })
-      .then((result) => {
-        console.log("Success:", result);
-        return {...state,
-          references: [result, ...state.references]
-        }
-      })
-
-  }
-}
-
-/**
- * Update existing Tracked Entity Instances
- * @example
- * execute(
- *   updateTEI(tei, data)
- * )(state)
- * @constructor
- * @param {object} tei - Payload data for the TEI to be updated
- * @param {object} data - Payload data for updating a TEI
- * @returns {Operation}
- */
-export function updateTEI(tei, data) {
-
-  return state => {
-    const body = expandReferences(data)(state);
-
-    const {
-      username,
-      password,
-      apiUrl
-    } = state.configuration;
-
-    const url = apiUrl.concat(`/api/trackedEntityInstances/${tei}`)
-
-    console.log(`Updating tracked entity instance ${tei} with data:`);
-    console.log(body)
-
-    return put({
-        username,
-        password,
-        body,
-        url
-      })
-      .then((result) => {
-        console.log("Success:", result);
-        return {...state,
-          references: [result, ...state.references]
-        }
-      })
-
-  }
-}
-
-// /**
-//  * Create and enroll TrackedEntityInstances
-//  * @example
-//  * execute(
-//  *   createEnrollTEI(te, orgUnit, attributes, enrollments)
-//  * )(state)
-//  * @constructor
-//  * @param {object} enrollmentData - Payload data for new enrollment
-//  * @returns {Operation}
-//  */
-// export function upsertEnroll(upsertData) {
-//
-//   return state => {
-//     const body = expandReferences(trackedEntityInstanceData)(state);
-//
-//     const { username, password, apiUrl } = state.configuration;
-//
-//     const url = resolveUrl(apiUrl + '/', 'api/trackedEntityInstances')
-//
-//     console.log("Posting tracked entity instance data:");
-//     console.log(body)
-//
-//     return post({ username, password, body, url })
-//     .then((result) => {
-//       console.log("Success:", result);
-//       return { ...state, references: [ result, ...state.references ] }
-//     })
-//
-//   }
-// }
-
-/**
- * Enroll a tracked entity instance in a program
- * @example
- * execute(
- *   enroll(enrollmentData)
- * )(state)
- * @constructor
- * @param {object} enrollmentData - Payload data for new enrollment
- * @returns {Operation}
- */
-export function enroll(tei, enrollmentData) {
-
-  return state => {
-    const body = expandReferences(enrollmentData)(state);
-    body["trackedEntityInstance"] = tei;
-
-    const {
-      username,
-      password,
-      apiUrl
-    } = state.configuration;
-
-    const url = resolveUrl(apiUrl + '/', 'api/enrollments')
-
-    console.log("Enrolling tracked entity instance with data:");
-    console.log(body)
-
-    return post({
+    return request.post({
         username,
         password,
         body,
