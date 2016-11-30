95a952fc8072052a74589c1b27a29e355443badf
taylordowns2000 <downs.taylor@gmail.com>
2016-11-30 11:49:04 +0700

    initial clone

---
+++ README.md
@@ -0,0 +1,88 @@
+Language DHIS2
+==============
+
+Language Pack for building expressions and operations for working with
+the [DHIS2 API](http://dhis2.github.io/dhis2-docs/master/en/developer/html/dhis2_developer_manual.html).
+
+Documentation
+-------------
+
+## Tracked Entity API
+
+#### Create a new tracked entity instance from a CommCare form submission.
+```js
+createTEI(
+  fields(
+    field("trackedEntity", "nEenWmSyUEp"),
+    field("orgUnit", "DiszpKrYNg8"),
+    field("attributes", function(state) {
+      return [{
+        "attribute": "w75KJ2mc4zz",
+        "value": dataValue("form.first_name")(state)
+      }, {
+        "attribute": "zDhUuAYrxNC",
+        "value": dataValue("form.last_name")(state)
+      }]
+    })
+  )
+)
+```
+
+## Events API
+
+#### `Events API` expression
+```js
+event(
+  fields(
+    field("program", "eBAyeGv0exc"),
+    field("orgUnit", "DiszpKrYNg8"),
+    field("eventDate", dataValue("date")),
+    field("status", "COMPLETED"),
+    field("storedBy", "admin"),
+    field("coordinate", {
+      "latitude": "59.8",
+      "longitude": "10.9"
+    }),
+    field("dataValues", function(state) {
+      return [
+        dataElement("qrur9Dvnyt5", dataValue("prop_a"))(state)
+        dataElement("oZg33kd9taw", dataValue("prop_b"))(state)
+        dataElement("msodh3rEMJa", dataValue("prop_c"))(state)
+      ]
+    })
+  )
+)
+```
+
+## Data Values / Data Value Sets API
+
+#### Current `DataValueSets API` expression
+```js
+dataValueSet(
+  fields(
+    field("dataSet", "pBOMPrpg1QX"),
+    field("orgUnit", "DiszpKrYNg8"),
+    field("period", "201401"),
+    field("completeData", dataValue("form.date")),
+    field("dataValues", function(state) {
+      return [
+        dataElement("qrur9Dvnyt5", dataValue("form.prop_a")(state)),
+        dataElement("oZg33kd9taw", dataValue("form.prop_b")(state)),
+        dataElement("msodh3rEMJa", dataValue("form.prop_c")(state))
+      ]
+    })
+  )
+)
+```
+
+[Docs](docs/index)
+
+
+Development
+-----------
+
+Clone the repo, run `npm install`.
+
+Run tests using `npm run test` or `npm run test:watch`
+
+Build the project using `make`.
---
+++ src/Adaptor.js
@@ -0,0 +1,313 @@
+import {
+  execute as commonExecute,
+  expandReferences
+} from 'language-common';
+import {
+  post,
+  put
+} from './Client';
+import {
+  resolve as resolveUrl
+} from 'url';
+
+/** @module Adaptor */
+
+/**
+ * Execute a sequence of operations.
+ * Wraps `language-common/execute`, and prepends initial state for DHIS2.
+ * @example
+ * execute(
+ *   create('foo'),
+ *   delete('bar')
+ * )(state)
+ * @constructor
+ * @param {Operations} operations - Operations to be performed.
+ * @returns {Operation}
+ */
+export function execute(...operations) {
+  const initialState = {
+    references: [],
+    data: null
+  }
+
+  return state => {
+    return commonExecute(...operations)({...initialState,
+      ...state
+    })
+  };
+
+}
+
+/**
+ * Create an event
+ * @example
+ * execute(
+ *   event(eventData)
+ * )(state)
+ * @constructor
+ * @param {object} eventData - Payload data for the event
+ * @returns {Operation}
+ */
+export function event(eventData) {
+
+  return state => {
+    const body = expandReferences(eventData)(state);
+
+    const {
+      username,
+      password,
+      apiUrl
+    } = state.configuration;
+
+    const url = resolveUrl(apiUrl + '/', 'api/events')
+
+    console.log("Posting event:");
+    console.log(body)
+
+    return post({
+        username,
+        password,
+        body,
+        url
+      })
+      .then((result) => {
+        console.log("Success:", result);
+        return {...state,
+          references: [result, ...state.references]
+        }
+      })
+
+  }
+}
+
+/**
+ * Send data values using the dataValueSets resource
+ * @example
+ * execute(
+ *   dataValueSet(data)
+ * )(state)
+ * @constructor
+ * @param {object} data - Payload data for the data value set
+ * @returns {Operation}
+ */
+export function dataValueSet(data) {
+
+  return state => {
+    const body = expandReferences(data)(state);
+
+    const {
+      username,
+      password,
+      apiUrl
+    } = state.configuration;
+
+    const url = resolveUrl(apiUrl + '/', 'api/dataValueSets')
+
+    console.log("Posting data value set:");
+    console.log(body)
+
+    return post({
+        username,
+        password,
+        body,
+        url
+      })
+      .then((result) => {
+        console.log("Success:", result);
+        return {...state,
+          references: [result, ...state.references]
+        }
+      })
+
+  }
+}
+
+/**
+ * Create a "dataElement" pairing for DHIS2.
+ * @example
+ * execute(
+ *   dataElement(key, value)
+ * )(state)
+ * @constructor
+ * @param {object} key - Payload data for the Data Element key
+ * @param {object} value - Payload data for the Data Element value
+ * @returns {Operation}
+ */
+export function dataElement(key, value) {
+  return {
+    "dataElement": key,
+    "value": value
+  }
+}
+
+/**
+ * Create one or many new Tracked Entity Instances
+ * @example
+ * execute(
+ *   createTEI(data)
+ * )(state)
+ * @constructor
+ * @param {object} data - Payload data for new tracked entity instance(s)
+ * @returns {Operation}
+ */
+export function createTEI(data) {
+
+  return state => {
+
+    const body = expandReferences(data)(state);
+
+    const {
+      username,
+      password,
+      apiUrl
+    } = state.configuration;
+
+    const url = resolveUrl(apiUrl + '/', 'api/trackedEntityInstances')
+
+    console.log("Posting tracked entity instance data:");
+    console.log(body)
+
+    return post({
+        username,
+        password,
+        body,
+        url
+      })
+      .then((result) => {
+        console.log("Success:", result);
+        return {...state,
+          references: [result, ...state.references]
+        }
+      })
+
+  }
+}
+
+/**
+ * Update existing Tracked Entity Instances
+ * @example
+ * execute(
+ *   updateTEI(tei, data)
+ * )(state)
+ * @constructor
+ * @param {object} tei - Payload data for the TEI to be updated
+ * @param {object} data - Payload data for updating a TEI
+ * @returns {Operation}
+ */
+export function updateTEI(tei, data) {
+
+  return state => {
+    const body = expandReferences(data)(state);
+
+    const {
+      username,
+      password,
+      apiUrl
+    } = state.configuration;
+
+    const url = apiUrl.concat(`/api/trackedEntityInstances/${tei}`)
+
+    console.log(`Updating tracked entity instance ${tei} with data:`);
+    console.log(body)
+
+    return put({
+        username,
+        password,
+        body,
+        url
+      })
+      .then((result) => {
+        console.log("Success:", result);
+        return {...state,
+          references: [result, ...state.references]
+        }
+      })
+
+  }
+}
+
+// /**
+//  * Create and enroll TrackedEntityInstances
+//  * @example
+//  * execute(
+//  *   createEnrollTEI(te, orgUnit, attributes, enrollments)
+//  * )(state)
+//  * @constructor
+//  * @param {object} enrollmentData - Payload data for new enrollment
+//  * @returns {Operation}
+//  */
+// export function upsertEnroll(upsertData) {
+//
+//   return state => {
+//     const body = expandReferences(trackedEntityInstanceData)(state);
+//
+//     const { username, password, apiUrl } = state.configuration;
+//
+//     const url = resolveUrl(apiUrl + '/', 'api/trackedEntityInstances')
+//
+//     console.log("Posting tracked entity instance data:");
+//     console.log(body)
+//
+//     return post({ username, password, body, url })
+//     .then((result) => {
+//       console.log("Success:", result);
+//       return { ...state, references: [ result, ...state.references ] }
+//     })
+//
+//   }
+// }
+
+/**
+ * Enroll a tracked entity instance in a program
+ * @example
+ * execute(
+ *   enroll(enrollmentData)
+ * )(state)
+ * @constructor
+ * @param {object} enrollmentData - Payload data for new enrollment
+ * @returns {Operation}
+ */
+export function enroll(tei, enrollmentData) {
+
+  return state => {
+    const body = expandReferences(enrollmentData)(state);
+    body["trackedEntityInstance"] = tei;
+
+    const {
+      username,
+      password,
+      apiUrl
+    } = state.configuration;
+
+    const url = resolveUrl(apiUrl + '/', 'api/enrollments')
+
+    console.log("Enrolling tracked entity instance with data:");
+    console.log(body)
+
+    return post({
+        username,
+        password,
+        body,
+        url
+      })
+      .then((result) => {
+        console.log("Success:", result);
+        return {...state,
+          references: [result, ...state.references]
+        }
+      })
+
+  }
+}
+
+export {
+  field,
+  fields,
+  sourceValue,
+  merge,
+  each,
+  dataPath,
+  dataValue,
+  lastReferenceValue
+}
+from 'language-common';
