a546707ab17222fa512a879bb360688a6ce0dc75
Taylor Downs <downs.taylor@gmail.com>
2018-05-15 08:39:37 +0000

    setup for tests

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 
 import Adaptor from '../src';
-const { execute, event, dataElement } = Adaptor;
+const { execute, submitSite } = Adaptor;
 
 import request from 'superagent';
 import superagentMock from 'superagent-mock';
-import ClientFixtures, { fixtures } from './ClientFixtures'
+import Fixtures, { fixtures } from './Fixtures'
 
 describe("execute", () => {
 
@@ -42,46 +42,38 @@ describe("execute", () => {
   })
 })
 
-describe("event", () => {
-  let mockRequest
-
-  before(() => {
-    mockRequest = superagentMock(request, ClientFixtures)
-  })
-
-  it("posts to API and returns state", () => {
-    let state = {
-      configuration: {
-        username: "hello",
-        password: "there",
-        apiUrl: 'https://play.resourcemap.org/demo'
-      }
-    };
-
-    return execute(
-      event(fixtures.event.requestBody)
-    )(state)
-    .then((state) => {
-      let lastReference = state.references[0]
-
-      // Check that the eventData made it's way to the request as a string.
-      expect(lastReference.params).
-        to.eql(JSON.stringify(fixtures.event.requestBody))
-
-    })
-
-  })
-
-  after(() => {
-    mockRequest.unset()
-  })
-
-})
-
-describe("dataElement", function() {
-  it("creates a on dataElement object object", function() {
-    let result = dataElement("key", function() { return "foo" })()
-
-    expect(result).to.eql({ dataElement: "key", value: "foo" })
-  })
-})
+// describe("submitSite", () => {
+//   let mockRequest
+//
+//   before(() => {
+//     mockRequest = superagentMock(request, Fixtures)
+//   })
+//
+//   it("posts to API and returns state", () => {
+//     let state = {
+//       configuration: {
+//         username: "hello",
+//         password: "there",
+//         apiUrl: 'https://www.resourcemap.org/demo'
+//       }
+//     };
+//
+//     return execute(
+//       submitSite(fixtures.site.id, fixtures.site.data)
+//     )(state)
+//     .then((state) => {
+//       let lastReference = state.references[0]
+//
+//       // Check that the eventData made it's way to the request as a string.
+//       expect(lastReference.params).
+//         to.eql(JSON.stringify(fixtures.event.requestBody))
+//
+//     })
+//
+//   })
+//
+//   after(() => {
+//     mockRequest.unset()
+//   })
+//
+// })
